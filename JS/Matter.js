8a0da47480b2a76ff9a5b9ab3fd93f88c3ccb168
Mratunjya Shakhdhar <mratunjya13@gmail.com>
2020-04-15 02:44:46 +0530

    Update Matter.js

--- JS/Matter.js
+++ JS/Matter.js
@@ -1,14 +1,20 @@
-x = document.getElementsByClassName('side-nav');
-console.log(x[0].offsetTop);
-h = x[0].offsetTop;
-window.onscroll = function() {myFunction()};
+document.onreadystatechange = () => {
+  if (document.readyState === 'complete') {
+    x = document.getElementsByClassName('side-nav');
+    console.log(x[0].offsetTop);
+    h = x[0].offsetTop;
+    window.onscroll = function() {myFunction()};
 
-function myFunction() {
-  if (document.body.scrollTop >= h || document.documentElement.scrollTop >= h) {
-    x[0].style.position = "fixed";
-    x[0].style.top = 0;
-  }
-  else {
-    x[0].style.position = "static";
+    function myFunction() {
+      if (document.body.scrollTop >= h || document.documentElement.scrollTop >= h) {
+        console.log(x[0].offsetTop);
+        x[0].style.position = "fixed";
+        x[0].style.top = 0;
+      }
+      else {
+        x[0].style.position = "static";
+      }
+    }
+    window.scrollBy(0, 1);
   }
 }
